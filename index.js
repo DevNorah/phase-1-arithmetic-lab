244b9cbcd1c49d84057ec1437b85dff08fb9fa5b
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-06-28 18:52:38 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply
+);
 
-// Write your code here
+// come back to the second one 
+const random = Math.floor (Math.random() * 10);
+
+
+
+//third one
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+// fourth one, still at it
+let max = Math.max(12, 7, 5, 20, 5)
